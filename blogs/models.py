5e0dfb3da303f1d3e942e66577f0c6da947db696
alexHooper <alexmohack@gmail.com>
2018-10-28 23:16:16 +0530

    create blog model

--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class Blog(models.Model):
+	owner = models.ForeignKey(User, on_delete=models.CASCADE)
+	title = models.CharField(max_length=250)
+	content = models.TextField()
+	timestamp = models.DateTimeField(auto_now_add=True)
+	updated = models.DateTimeField(auto_now=True)
