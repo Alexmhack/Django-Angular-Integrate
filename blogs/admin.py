4a69e1f0097063f31166544dad19d46959519561
alexHooper <alexmohack@gmail.com>
2018-10-29 09:45:21 +0530

    register blog model to admin

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Blog
+
+@admin.register(Blog)
+class BlogModelAdmin(admin.ModelAdmin):
+	list_display = ('owner', 'title', 'timestamp')
+	search_fields = ('owner', 'title', 'content')
+	list_filter = ('owner',)
+	list_editable = ('title',)
