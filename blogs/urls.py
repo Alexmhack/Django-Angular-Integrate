4c60eb134fa16c6b2997280c941f5c76b17f0621
alexHooper <alexmohack@gmail.com>
2018-10-29 15:51:22 +0530

    add index view

--- blogs/urls.py
+++ blogs/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path, include
 
+from .views import index_view
+
 app_name = 'blogs'
 
 urlpatterns = [
 	path('api/', include('blogs.api.urls', namespace='blogs-api')),
+	path('', index_view, name='index'),
 ]
