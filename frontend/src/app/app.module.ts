493820d49c6f9b377d5465313710dc531945e41a
alexHooper <alexmohack@gmail.com>
2018-10-29 10:02:18 +0530

    generate blogs component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BlogsComponent } from './blogs/blogs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlogsComponent
   ],
   imports: [
     BrowserModule,
