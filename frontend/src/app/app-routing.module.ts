43c8adf8120881afaa0e5e68f9a61a480ed753ce
alexHooper <alexmohack@gmail.com>
2018-10-29 22:50:16 +0530

    add routes for angular app

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { BlogsComponent } from './blogs/blogs.component';
+import { BlogDetailComponent } from './blog-detail/blog-detail.component';
+
+const routes: Routes = [
+	{ path: '', component: BlogsComponent },
+	{ path: 'blog/:id', component: BlogDetailComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
