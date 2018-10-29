0c3cffc6cd5c2fd9ac2abf17b9c9cb067996e6ef
alexHooper <alexmohack@gmail.com>
2018-10-29 22:52:37 +0530

    fetch and show blog details

--- frontend/src/app/blog-detail/blog-detail.component.ts
+++ frontend/src/app/blog-detail/blog-detail.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { Blog } from '../shared/blog';
+import { BlogService } from '../core/blog.service';
 
 @Component({
   selector: 'app-blog-detail',
@@ -7,9 +12,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BlogDetailComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+  	private blogService: BlogService,
+  	private route: ActivatedRoute,
+  	private location: Location
+  ) { }
+
+  blog: Blog;
 
   ngOnInit() {
+  	this.getBlog();
+  }
+
+  getBlog() {
+  	const id = +this.route.snapshot.paramMap.get('id')
+  	this.blogService.getBlog(id)
+  		.subscribe(blog => this.blog = blog)
+  }
+
+  goBack(): void {
+  	this.location.back();
   }
 
 }
