f1baee41a6f2dc6170d47b53faf1d9bcbbaf23a4
alexHooper <alexmohack@gmail.com>
2018-10-29 08:31:21 +0530

    add account model serializer

--- accounts/api/serializers.py
+++ accounts/api/serializers.py
@@ -1,1 +1,10 @@
+from rest_framework import serializers
 
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class AccountModelSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = User
+		fields = ('username', 'email')
