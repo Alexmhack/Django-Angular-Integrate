9e57279246f44cb5020022c475440d991beba247
alexHooper <alexmohack@gmail.com>
2018-10-29 17:23:40 +0530

    create blog list viewset

--- blogs/api/routers.py
+++ blogs/api/routers.py
@@ -2,6 +2,8 @@ from rest_framework import routers
 
 from .viewsets import BlogViewSet
 
+app_name = 'blogs-router'
+
 router = routers.SimpleRouter()
 
 router.register('', BlogViewSet, basename='list')
--- blogs/api/viewsets.py
+++ blogs/api/viewsets.py
@@ -1,1 +1,10 @@
 from rest_framework import viewsets
+from rest_framework import permissions
+
+from blogs.models import Blog
+from .serializers import BlogModelSerializer
+
+class BlogViewSet(viewsets.ModelViewSet):
+	queryset = Blog.objects.all()
+	serializer_class = BlogModelSerializer
+	permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
