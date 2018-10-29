eafb2873d85e33e7870f948ef3e84fd0c7455784
alexHooper <alexmohack@gmail.com>
2018-10-29 08:27:09 +0530

    add blog model serializer

---
+++ blogs/api/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+
+from blogs.models import Blog
+from accounts.api.models import AccountModelSerializer
+
+class BlogModelSerializer(serializers.ModelSerializer):
+	owner = AccountModelSerializer(read_only=True)
+
+	class Meta:
+		model = Blog
+		fields = ('owner', 'title', 'content', 'timestamp', 'updated')
+
+	def get_updated(self, obj):
+		if obj.timestamp == obj.updated:
+			return None
+		return obj.updated.strftime("%b %d, %Y at %H:%M %p")
+
+	def get_timestamp(self, obj):
+		return obj.timestamp.strftime("%b %d, %Y at %H:%M %p")
