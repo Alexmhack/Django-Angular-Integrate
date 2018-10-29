e5d77fbf2b5a31aa41ac0bdd723332bf031f880e
alexHooper <alexmohack@gmail.com>
2018-10-29 13:20:10 +0530

    configure heroku with django

--- backend/settings.py
+++ backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 from decouple import config
 
@@ -153,3 +154,8 @@ REST_FRAMEWORK = {
 CORS_ORIGIN_WHITELIST = (
     'localhost:4200',
 )
+
+
+# Activate Django-Heroku.
+
+django_heroku.settings(locals())
