f98c5d1505315826d38f9f407d20fa067f321b94
alexHooper <alexmohack@gmail.com>
2018-10-29 16:24:54 +0530

    change into production mode

--- backend/settings.py
+++ backend/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('PROJECT_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['evening-falls-50549.herokuapp.com']
 
