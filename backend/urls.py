718dbc0cccaeeb0390a6dbe482e6bb5bf6890acf
alexHooper <alexmohack@gmail.com>
2018-10-29 22:56:21 +0530

    redirect the root url to blogs

--- backend/urls.py
+++ backend/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -7,5 +8,6 @@ urlpatterns = [
 
 # blogs app urls
 urlpatterns += [
+    path('', RedirectView.as_view(url='/blogs/'), name='blogs-redirect')
     path('blogs/', include('blogs.urls', namespace='blogs')),
 ]
